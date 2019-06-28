6f5f14f1cd7d0344e59d1993244fe66db687fb98
Nimit Shah <nimit199@gmail.com>
2019-06-28 16:06:47 +0530

    JSON FILE CREATED

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace EmpWebSystem.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+
             return View();
         }
 
